baddb8fc6c8c9e8fb5f244e15bc7e55d009c7c3f
Kevin <kwcw@live.com>
2023-02-20 22:02:36 +1100

    Just doing a front end standalone

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  public forecasts?: WeatherForecast[];
+
+  constructor(http: HttpClient) {
+  }
+
+  title = 'MyFrontEnd';
+}
+
+interface WeatherForecast {
+  date: string;
+  temperatureC: number;
+  temperatureF: number;
+  summary: string;
+}
